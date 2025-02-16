90c5e5cc31920cbc28870723c50df294752b332c
Your Name <you@example.com>
2025-02-16 09:25:54 +0700

    Đẩy mã nguồn lên GitHub

--- truclycoffe-be/models/FoodItem.js
+++ truclycoffe-be/models/FoodItem.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
-const FoodItemSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: true },
-    type: { type: String, default: "food" },
-  },
-  { collection: "shopfood" }
-);
+const FoodItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  type: { type: String, required: true, default: "food" },
+});
 
 module.exports = mongoose.model("FoodItem", FoodItemSchema);
--- truclycoffe-be/models/WaterItem.js
+++ truclycoffe-be/models/WaterItem.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
-const WaterItemSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: true },
-    type: { type: String, default: "water" },
-  },
-  { collection: "shopwater" }
-);
+const WaterItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  type: { type: String, required: true, default: "water" },
+});
 
 module.exports = mongoose.model("WaterItem", WaterItemSchema);
